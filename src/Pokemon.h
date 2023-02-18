c743487854c62234f210c85acf735374f260e9f4
thibaudfriche <thibaud.friche@gmail.com>
2023-02-18 10:11:34 +0100

    Saved changes.

--- src/Pokemon.h
+++ src/Pokemon.h
@@ -4,4 +4,17 @@
 
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{   
+    private : 
+        std::string _name;
+
+    public :
+        Pokemon(std::string name) 
+        : _name {name}
+        {}
+
+        std::string name() const{
+            return _name;
+        }
+        
+};
